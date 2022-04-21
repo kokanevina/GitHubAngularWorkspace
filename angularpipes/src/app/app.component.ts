ccfc5be87952a5a1ab4b0221f90325b2fff1bf19
kokanevina <kokane.vina@gmail.com>
2022-04-21 15:54:08 +0530

    pipes
    pipes

---
+++ angularpipes/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'anGularPipes';
+  message="welcome to neosoft technologies";
+  message1="welcome-to-neosoft-technologies";
+}
