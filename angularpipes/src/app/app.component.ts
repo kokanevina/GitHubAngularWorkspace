3b7ef4f13bef6074e4fb5577456a1bd7592cc813
kokanevina <kokane.vina@gmail.com>
2022-04-21 17:02:01 +0530

    Angular Pipes
    till Date pipe

--- angularpipes/src/app/app.component.ts
+++ angularpipes/src/app/app.component.ts
@@ -9,4 +9,14 @@ export class AppComponent {
   title = 'anGularPipes';
   message="welcome to neosoft technologies";
   message1="welcome-to-neosoft-technologies";
+  
+  sliceObj={
+    startIndex:-10,
+    endIndex:-2
+  }
+  
+  salary=67890;
+  salaries=[78906.64793, 45005.3167, 78999.234,65677.67, 56004.5,67888]
+
+  today=new Date();
 }
